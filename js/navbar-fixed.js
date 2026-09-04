016d10f1ab2579772bbdb6747a83d50ddc9c0792
Efrén Hernández <140679019+efrenhdez@users.noreply.github.com>
2026-09-03 23:18:43 -0600

    Add files via upload

---
+++ js/navbar-fixed.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let navbar = document.querySelector(".menu__navbar");
+
+    window.addEventListener("scroll", () =>{
+        let scroll = window.scrollY;
+
+        if(scroll > 150){
+            navbar.classList.add("menu__navbar--fixed");
+        }else{
+             navbar.classList.remove("menu__navbar--fixed");
+        }
+
+    });
+
+});
