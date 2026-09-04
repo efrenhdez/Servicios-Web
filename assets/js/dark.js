7ce6590a7454cba6ab7888f3f87756da6b82d0f8
Efrén Hernández <140679019+efrenhdez@users.noreply.github.com>
2026-09-03 23:25:35 -0600

    Add files via upload

---
+++ assets/js/dark.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let switcher = document.querySelector(".switcher__btn");
+    let light = document.querySelector(".swithcer__icon-light");
+    let dark = document.querySelector(".swithcer__icon-dark");
+
+    switcher.addEventListener("click", () =>{
+       let head = document.head;
+
+       let link = document.createElement("link");
+       link.rel = "stylesheet";
+       link.type = "text/css";
+       link.href = "./assets/css/dark.css";
+       link.id = "theme-dark";
+
+       let theme_dark = document.querySelector("#theme-dark");
+
+       if(theme_dark){
+            head.removeChild(theme_dark);
+            light.style.display = "block";
+            dark.style.display = "none";
+
+       }else{
+          head.appendChild(link);  
+          light.style.display = "none";
+          dark.style.display = "block";
+       }
+
+    });
+
+});
